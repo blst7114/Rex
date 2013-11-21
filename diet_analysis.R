017af1e118e3c63351813fc0235699b4928361ca
mbjoseph <maxwellbjoseph@gmail.com>
2013-11-21 15:59:44 -0700

    first upload of analysis

---
+++ diet_analysis.R
@@ -0,0 +1,22 @@
+# Doing some simple analysis of the cascades frog diet dataset
+# for details see Joseph et al. 2011 Freshwater Biology
+
+# load packages
+library(lme4)
+library(ggplot2)
+library(effects)
+
+# load in data
+diet <- read.csv("diet.csv", header=T)
+str(diet)
+
+# test whether frogs at lakes with trout have a lower probability of consuming aquatic prey
+# binomial mixed model, nested random effects (individuals within lakes)
+model <- glmer(aquatic ~ fish + (1|lake) + (1|frog_id), data=diet, family=binomial)
+model
+effs <- allEffects(model)
+plot(effs, rescale.axis=F, rug=F,
+     main="Effects of non-native trout", 
+     xlab="Trout absence or presence (0 or 1)", 
+     ylab="Pr(consumed prey is aquatic)", 
+     ylim=c(0, 1))
